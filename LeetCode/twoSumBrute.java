1e8639272c472a955658d6c7456bba29bf4e4fa1
Priyam Garg <gargpriyam21@gmail.com>
2020-06-19 17:02:51 +0530

    Video Updates

---
+++ LeetCode/twoSumBrute.java
@@ -0,0 +1,17 @@
+//Brute Force
+class twoSumBrute {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[j]==target-nums[i])
+                {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No Two add up to the TargetSum");
+    }
+    
+}
